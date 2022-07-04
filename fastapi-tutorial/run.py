8bf8cf39c2fa3eb9d186b5143628e97d5df44f35
yumingmin <yumingmin@xinye.com>
2022-07-05 00:11:35 +0800

    更新: 第三章 请求参数和验证

--- fastapi-tutorial/run.py
+++ fastapi-tutorial/run.py
@@ -7,16 +7,15 @@
 # ================================
 import uvicorn
 from fastapi import FastAPI
-
 from tutorial import app03, app04, app05
 
 
 app = FastAPI()
 app.include_router(app03, prefix="/ch03", tags=["第三章 请求参数和验证"])
 app.include_router(app04, prefix="/ch04", tags=["第四章 响应处理和FastAPI配置"])
-app.include_router(app05, prefix="/ch05", tags=["第三章 FastAPI的依赖注入系统"])
+app.include_router(app05, prefix="/ch05", tags=["第五章 FastAPI的依赖注入系统"])
 
 
 if __name__ == '__main__':
-    # 使用 uvicorn 运行服务
-    uvicorn.run("run:app", host="127.0.0.1", port=8000, reload=True, debug=True, workers=2)
+    # 使用 uvicorn 运行服务: uvicorn run:app --reload
+    uvicorn.run("run:app", host="127.0.0.1", port=8000, reload=True, debug=True, workers=1)
--- fastapi-tutorial/tutorial/ch03.py
+++ fastapi-tutorial/tutorial/ch03.py
@@ -5,6 +5,50 @@
 # Create Time: 2022/7/4 18:31:00
 # Update Time:
 # ================================
-from fastapi import APIRouter
+"""路径参数和数字验证 Path Parameters and Number Validations
+- 函数的顺序就是路由的顺序
+- 当请求为 /path/parameters 时，哪个函数在前就匹配哪个
+"""
+from fastapi import APIRouter, Path
+from enum import Enum
 
 app03 = APIRouter()
+
+
+@app03.get("/path/{parameters}")
+async def path_params01(parameters: str):
+    return {"message": parameters}
+
+
+@app03.get("/path/parameters")
+async def path_params01():
+    return {"message": "This is a message!"}
+
+
+class CityName(str, Enum):
+    Beijing = "Beijing China"
+    Shanghai = "Shanghai China"
+
+
+@app03.get("/enum/{city}")
+async def latest(city: CityName):
+    if city == CityName.Shanghai:
+        return {"city_name": city, "confirmed": 1492, "death": 7}
+
+    if city == CityName.Beijing:
+        return {"city_name": city, "confirmed": 1201, "death": 10}
+
+    return {"city_name": city, "latest": "unknown"}
+
+
+# 加上 path 则可以接收 Linux 路径
+@app03.get("/files/{file_path:path}")
+async def filepath(file_path: str):
+    return f"The file path is  {file_path}"
+
+
+@app03.get("/path2/{num}")
+async def path_params_validate(
+        num: int = Path(..., ge=1, le=10, title="Your number", description="输入文件夹数字"),
+):
+    return num
