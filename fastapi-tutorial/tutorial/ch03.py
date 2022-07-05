b198871a3f95c15f8b87176e90b5f336b8992a25
yumingmin <yumingmin@xinye.com>
2022-07-05 08:20:03 +0800

    更新: 查询参数和字符串验证

--- fastapi-tutorial/tutorial/ch03.py
+++ fastapi-tutorial/tutorial/ch03.py
@@ -5,10 +5,12 @@
 # Create Time: 2022/7/4 18:31:00
 # Update Time:
 # ================================
-"""路径参数和数字验证 Path Parameters and Number Validations
+"""路径参数和数字验证 (Path Parameters and Number Validations)
 - 函数的顺序就是路由的顺序
 - 当请求为 /path/parameters 时，哪个函数在前就匹配哪个
 """
+from typing import Optional
+
 from fastapi import APIRouter, Path
 from enum import Enum
 
@@ -47,8 +49,31 @@ async def filepath(file_path: str):
     return f"The file path is  {file_path}"
 
 
+# Path(...) 中的 3 个点号表示必填
 @app03.get("/path2/{num}")
 async def path_params_validate(
         num: int = Path(..., ge=1, le=10, title="Your number", description="输入文件夹数字"),
 ):
     return num
+
+
+"""查询参数和字符串验证(Query Parameters and String Validations)
+
+"""
+
+
+@app03.get("/query")
+async def page_limit(page: int = 1, limit: Optional[int] = None):
+    if limit:
+        return {"page": page, "limit": limit}
+
+    return {"page": page}
+
+
+@app03.get("/query/bool/conversion")
+async def type_conversion(param: bool = False):
+    """传入的如果是 TRUE / FALSE / true / false / 0 / 1 都可以正常被解析
+    如果传入的非上面值，则会报错
+    """
+    return param
+
--- fastapi-tutorial/tutorial/ch04.py
+++ fastapi-tutorial/tutorial/ch04.py
@@ -5,6 +5,9 @@
 # Create Time: 2022/7/4 18:31:00
 # Update Time:
 # ================================
+"""查询参数和数据的解析、验证
+
+"""
 from fastapi import APIRouter
 
 app04 = APIRouter()
